c0686c6f07356a982c2ca1a46df26838b52b9036
Jeremy Englert <jeremy@theadgarage.com>
2014-02-09 21:23:18 -0700

    Updated to Foundation 5.1.1

--- sidebar-offcanvas.php
+++ sidebar-offcanvas.php
@@ -1,8 +1,8 @@
-<div id="sidebar1" class="sidebar large-4 medium-4 columns" role="complementary">
+<div id="sidebar-offcanvas" class="sidebar large-4 medium-4 columns" role="complementary">
 
-	<?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
+	<?php if ( is_active_sidebar( 'offcanvas' ) ) : ?>
 
-		<?php dynamic_sidebar( 'sidebar1' ); ?>
+		<?php dynamic_sidebar( 'offcanvas' ); ?>
 
 	<?php else : ?>
 
