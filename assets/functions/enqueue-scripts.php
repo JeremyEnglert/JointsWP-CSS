c08e941fbaa34b5be776ec5c6bab5f580dadd11e
Jeremy Englert <jeremy@cmsadgroup.com>
2015-06-12 19:27:53 -0700

    Added normalize css to head

--- assets/functions/enqueue-scripts.php
+++ assets/functions/enqueue-scripts.php
@@ -7,10 +7,10 @@ function joints_scripts_and_styles() {
 	// Removes WP version of jQuery
 	wp_deregister_script('jquery');
 	
-	// Loads jQuery from bower_components
+	// Loads jQuery from vendor Files
     wp_enqueue_script( 'jquery', get_template_directory_uri() . '/assets/vendor/foundation/js/vendor/jquery.js', array(), '2.1.3', true );
     
-    // Modernizr from bower_components
+    // Modernizr from vendor Files
     wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/assets/vendor/foundation/js/vendor/modernizr.js', array(), '2.8.3', true );
     
     // Adding Foundation scripts file in the footer
@@ -18,9 +18,12 @@ function joints_scripts_and_styles() {
    
     // Adding scripts file in the footer
     wp_enqueue_script( 'site-js', get_template_directory_uri() . '/assets/js/scripts.min.js', array( 'jquery' ), '', true );
-       
+    
+    // Normalize from vendor files
+    wp_enqueue_style( 'normalize-css', get_template_directory_uri() . '/assets/vendor/foundation/css/normalize.min.css', array(), '', 'all' );      
+    
     // Adding Foundation styles
-    wp_enqueue_style( 'foundation-css', get_template_directory_uri() . '/assets/vendor/foundation/css/foundation.css', array(), '', 'all' );
+    wp_enqueue_style( 'foundation-css', get_template_directory_uri() . '/assets/vendor/foundation/css/foundation.min.css', array(), '', 'all' );
    
     // Register main stylesheet
     wp_enqueue_style( 'site-css', get_template_directory_uri() . '/assets/css/style.css', array(), '', 'all' );
