6deef0f52bef069f50bda2fd739b72293935ab3d
Jeremy Englert <jeremyenglert@Jeremys-MacBook-Pro.local>
2018-10-14 22:34:06 -0700

    Catch CSS version up with Sass version

--- parts/content-offcanvas.php
+++ parts/content-offcanvas.php
@@ -8,4 +8,11 @@
 
 <div class="off-canvas position-right" id="off-canvas" data-off-canvas>
 	<?php joints_off_canvas_nav(); ?>
+
+	<?php if ( is_active_sidebar( 'offcanvas' ) ) : ?>
+
+		<?php dynamic_sidebar( 'offcanvas' ); ?>
+
+	<?php endif; ?>
+
 </div>
